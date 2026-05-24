08e4201b14fe8c4e80e8f038edfd8ad6af11c1f7
Aman Vyas <vyasaman1610@gmail.com>
2026-05-24 12:30:00 +0530

    Initial Commit

---
+++ PracticePrograms.py
@@ -0,0 +1,17 @@
+# Practice Problem: Print a text in python .
+'''
+print("Hello")
+print("This is my first program in python")
+'''
+######################################################################
+
+# Practice Problem: Given two strings, s1 and s2, create a new string by appending s2 in the middle of s1.
+'''
+str1 = input("Enter first string ")
+str2 = input("Enter second string ")
+l = len(str1)
+m = l//2
+print(str1[0:m]+str2+str1[m:l])
+'''
+######################################################################
+
